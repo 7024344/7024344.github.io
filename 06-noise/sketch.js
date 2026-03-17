401372181e5eb32bb89a38a6993624ef0e3da072
fifa <7024344@spsd.sk.ca>
2026-03-17 14:36:49 -0600

    noise demo

---
+++ 06-noise/sketch.js
@@ -0,0 +1,19 @@
+// Perlin Noise Demo
+// March 17/2026
+
+let time = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  fill("black");
+  let x = noise(time) * width;
+  let y = noise(time + 1000) * height;
+  circle(x, y, 75);
+
+  time += 0.01;
+}
