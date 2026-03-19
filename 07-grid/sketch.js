614e8b6e1c1bde5c820393088949e72d383e2fdb
fifa <7024344@spsd.sk.ca>
2026-03-19 13:04:31 -0600

    grid

--- 07-grid/sketch.js
+++ 07-grid/sketch.js
@@ -1,13 +1,15 @@
 // 
 // 
 
-let theGrid = [[1, 0, 0, 0],
-               [1, 0, 1, 0],
-               [0, 1, 0, 0],
-               [0, 0, 1, 1]];
+// let theGrid = [[1, 0, 0, 0],
+              //  [1, 0, 1, 0],
+              //  [0, 1, 0, 0],
+              //  [0, 0, 1, 1]];
 
-const SQUARE_DIMENSION = theGrid.length;
+// const SQUARE_DIMENSION = theGrid.length;
 
+const SQUARE_DIMENSION = 10;
+let theGrid;
 let cellSize;
 
 function setup() {
@@ -18,12 +20,28 @@ function setup() {
   else {
     cellSize = width/SQUARE_DIMENSION;
   }
-  
+  theGrid = randomizeGrid
 }
 
 function draw() {
   background(220);
   showGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (theGrid[y][x] === 1) {
+    theGrid[y][x] = 0;
+  }
+  else if (theGrid[y][x] === 0) {
+    theGrid[y][x] = 1;
+  }
 }
 
 function showGrid() {
@@ -36,6 +54,21 @@ function showGrid() {
         fill("while");
       }
       square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function randomizeGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid,push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[x].push(1);
+      }
     }
   }
 }
